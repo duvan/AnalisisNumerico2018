998ba0efc6a80f764a30734bc47434f1471170dc
duvanbelo@gmail.com <swordfish147/>
2018-09-16 12:00:05 -0500

    first commit with menu working

---
+++ src/analisisnumerico2018/AnalisisNumerico2018.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package analisisnumerico2018;
+
+import Views.AnalisisNumericoView;
+
+/**
+ *
+ * @author duvandariobarrientosbetancur
+ */
+public class AnalisisNumerico2018 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new AnalisisNumericoView().setVisible(true);
+    }
+}
